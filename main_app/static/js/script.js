97f68c9ed39302d12dc808b8df2b87b71b5835f8
Jay Park <bhj0308@gmail.com>
2021-02-05 09:52:53 -0500

    deploy fix

--- main_app/static/js/script.js
+++ main_app/static/js/script.js
@@ -5,5 +5,6 @@ function overlay() {
 
 function overlay2() {
 	el = document.getElementById("overlay2");
+	console.log(el)
 	el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
 }
